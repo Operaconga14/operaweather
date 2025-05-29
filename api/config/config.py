d984c4cea599e9910b7cf1131e7f453be0306416
Amire Joseph <amirejoseph83@gmail.com>
2025-05-29 17:43:32 +0100

    Starting again to troubleshoot

---
+++ api/config/config.py
@@ -0,0 +1,8 @@
+from fastapi import FastAPI
+import os
+from dotenv import load_dotenv
+
+load_dotenv
+
+API_URL = os.getenv("API_URL")
+OPEN_WEATHER_API_KEY = os.getenv("OPEN_WEATHER_API_KEY")
--- api/vercel.py
+++
@@ -1,5 +0,0 @@
-from mangum import Adapter
-from .index import app
-
-# Handler for Vercel
-handler = Adapter(app) 
