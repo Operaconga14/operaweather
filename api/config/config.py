3dea02b56e1ea7d2d0f29204e328a36feb993d27
Amire Joseph <amirejoseph83@gmail.com>
2025-05-29 20:50:33 +0100

    Update

--- api/config/config.py
+++ api/config/config.py
@@ -1,12 +1,10 @@
 from fastapi import FastAPI, HTTPException, status
 import os
 from dotenv import load_dotenv
-from pyowm import OWM
+import requests
+
 
 load_dotenv()
 
 API_URL = os.getenv("API_URL")
 OPEN_WEATHER_API_KEY = os.getenv("OPEN_WEATHER_API_KEY").strip('"')
-
-owm = OWM(OPEN_WEATHER_API_KEY)
-mgr = owm.weather_manager()
--- api/index.py
+++ api/index.py
@@ -1,4 +1,4 @@
-from .config.config import FastAPI, API_URL, mgr, OWM, HTTPException, status
+from .config.config import FastAPI, API_URL, HTTPException, status
 
 app = FastAPI(
     title="Weather API",
@@ -10,75 +10,3 @@ app = FastAPI(
 @app.get("/")
 def root():
     return {"message": "Welcome to the Weather API"}
-
-@app.get(f"/{API_URL}/weather")
-def get_weather(city: str):
-    try:
-        weather = mgr.weather_at_place(city).weather
-        return  {
-            "status": "success",
-            "data": {
-                "temperature": {
-                    "current": weather.temperature('celsius')['temp'],
-                    "feels_like": weather.temperature('celsius')['feels_like']
-                },
-                "humidity": weather.humidity,
-                "description": weather.detailed_status,
-                "wind_speed": weather.wind()['speed'],
-                "clouds": weather.clouds,
-                "timestamp": datetime.fromtimestamp(weather.reference_time()).isoformat()
-            }
-        }
-    except Exception as e:
-        raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e)
-        )
-
-
-@app.get(f"/{API_URL}/weather/temperature")
-def get_temperature(city: str):
-    try:
-        weather = mgr.weather_at_place(city).weather
-        return {
-            "status": "success",
-            "data": {
-                "city": city,
-                "temperature": weather.temperature("celsius")["temp"],
-            },
-        }
-    except Exception as e:
-        raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e)
-        )
-
-@app.get(f"/{API_URL}/weather/humidity")
-def get_humidity(city: str):
-    try:
-        weather = mgr.weather_at_place(city).weather
-        return {
-            "status": "success",
-            "data": {
-                "city": city,
-                "humidity": weather.humidity,
-            },
-        }
-    except Exception as e:
-        raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e)
-        )
-
-@app.get(f"/{API_URL}/weather/wind")
-def get_wind(city: str):
-    try:
-        weather = mgr.weather_at_place(city).weather
-        return {
-            "status": "success",
-            "data": {
-                "city": city,
-                "wind_speed": weather.wind()["speed"],
-            },
-        }
-    except Exception as e:
-        raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e)
-        )
